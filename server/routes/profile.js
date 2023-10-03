9a4b25206d0c69a9f8c5ecaf9c935242fcea64c9
Mora <mlabisi98@gmail.com>
2023-10-02 19:24:32 -0700

    Update profile.js
    Correcting Angel's name

--- server/routes/profile.js
+++ server/routes/profile.js
@@ -10,7 +10,7 @@ const fakeProfiles = [{
 },
     {
         sellerId: '2',
-        name: 'Angela Gadon',
+        name: 'Angel Gadon',
         pic: 'https://cdn.shopify.com/s/files/1/1061/1924/products/Smirk_Face_Emoji_large.png?v=1571606036',
         rating: '★★★★☆',
         specs: 'KBB/AT Specs',
